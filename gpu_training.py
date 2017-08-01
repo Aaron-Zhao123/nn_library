8680635beb84c08202b6d7c42095ed5ad25a5705
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-02 00:36:16 +0100

    trian from scrach

--- gpu_training.py
+++ gpu_training.py
@@ -83,7 +83,7 @@ def tower_loss(scope, isTrain, isLoad):
 
   # with tf.control_dependencies([loss_averages_op]):
   total_loss = tf.identity(total_loss)
-  return (total_loss, test_acc, top5)
+  return (total_loss, test_acc, top5, logits)
 
 
 def average_gradients(tower_grads):
@@ -173,7 +173,7 @@ def train():
         with tf.device('/gpu:%d' % i):
           with tf.name_scope('%s_%d' % (model_wrapper.TOWER_NAME, i)) as scope:
             # loss for one tower.
-            loss, tower_top1_acc, tower_top5_acc = tower_loss(scope, isTrain_ph, isLoad)
+            loss, tower_top1_acc, tower_top5_acc, logits= tower_loss(scope, isTrain_ph, isLoad)
             # Reuse variables for the next tower.
             tf.get_variable_scope().reuse_variables()
             # Retain the summaries from the final tower.
@@ -251,9 +251,10 @@ def train():
       step = 0
 
       while (step <= train_epoch_size and FLAGS.is_train):
-        _, loss_value = sess.run([train_op, loss], feed_dict = {isTrain_ph:FLAGS.is_train})
+        _, loss_value, logits_value= sess.run([train_op, loss, logits], feed_dict = {isTrain_ph:FLAGS.is_train})
         step += FLAGS.batch_size * FLAGS.num_gpus
         print(loss_value)
+        print(logits_value)
         assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
         if (step % 100 == 0):
           train_bar.update(step)
