84a3f734efa9593e75421ef1bd775bfe1ddaab5a
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-02 00:59:40 +0100

    fixed last layer

--- gpu_training.py
+++ gpu_training.py
@@ -254,7 +254,7 @@ def train():
         _, loss_value, logits_value= sess.run([train_op, loss, logits], feed_dict = {isTrain_ph:FLAGS.is_train})
         step += FLAGS.batch_size * FLAGS.num_gpus
         # print(loss_value)
-        # print(logits_value)
+        print(logits_value)
         assert not np.isnan(np.min(logits_value)), 'Model diverged with logits value have NaN'
         assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
         if (step % 100 == 0):
