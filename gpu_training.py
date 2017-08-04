b50ad76b7c3c96c68de1c69e414dde8b0775c08f
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-05 00:02:05 +0100

    use 1 gpu to train- test run

--- gpu_training.py
+++ gpu_training.py
@@ -251,12 +251,12 @@ def train():
       start_time = time.time()
       step = 0
 
-      while (step <= train_epoch_size and FLAGS.is_train):
-        # _, loss_value = sess.run([train_op, loss], feed_dict = {isTrain_ph:FLAGS.is_train})
-        step += FLAGS.batch_size * FLAGS.num_gpus
-        assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-        if (step % 100 == 0):
-          train_bar.update(step)
+    #   while (step <= train_epoch_size and FLAGS.is_train):
+    #     _, loss_value = sess.run([train_op, loss], feed_dict = {isTrain_ph:FLAGS.is_train})
+    #     step += FLAGS.batch_size * FLAGS.num_gpus
+    #     assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+    #     if (step % 100 == 0):
+    #       train_bar.update(step)
 
       if FLAGS.is_train:
         duration = time.time() - start_time
