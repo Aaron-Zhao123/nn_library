29039f026de0df6ae23bbb9713a73cd4a8676096
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-05 10:11:21 +0100

    testing loss

--- gpu_training.py
+++ gpu_training.py
@@ -162,7 +162,7 @@ def train():
       # Create an optimizer that performs gradient descent.
       opt = tf.train.GradientDescentOptimizer(lr)
     else:
-      opt = tf.train.AdamOptimizer(1e-3)
+      opt = tf.train.AdamOptimizer(1e-1)
 
     # Calculate the gradients for each model tower.
     tower_grads = []
