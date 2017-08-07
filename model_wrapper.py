9fdc34a5b0d645017de2329238111f313e357fcb
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-07 16:50:20 +0100

    train vgg

--- model_wrapper.py
+++ model_wrapper.py
@@ -28,9 +28,9 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = ult.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
-NUM_EPOCHS_PER_DECAY = 6.0      # Epochs after which learning rate decays.
-LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.01       # Initial learning rate.
+NUM_EPOCHS_PER_DECAY = 1.0      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.75  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
 TOWER_NAME = 'tower'
 
 def _activation_summary(x):
