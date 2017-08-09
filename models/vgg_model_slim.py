75181de7f91446e971574bef9ca7b9622bc1d7af
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-09 01:25:55 +0100

    slim train try

--- models/vgg_model_slim.py
+++ models/vgg_model_slim.py
@@ -55,8 +55,9 @@ def vgg_16(inputs,
     with arg_scope(
             [slim.conv2d, slim.fully_connected, slim.max_pool2d],
             outputs_collections=end_points_collection):
+        net = inputs
         net = slim.repeat(
-            inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+            net, 2, slim.conv2d, 64, [3, 3], scope='conv1')
         net = slim.max_pool2d(net, [2, 2], scope='pool1')
         net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
         net = slim.max_pool2d(net, [2, 2], scope='pool2')
