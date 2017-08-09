bce5a92897162f7c4e41607e55a514a9de5753b5
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-09 02:05:57 +0100

    slim train try

--- model_wrapper_slim.py
+++ model_wrapper_slim.py
@@ -75,9 +75,7 @@ def loss(logits, labels, batch_size=None):
                                     1.0, 0.0)
     # Cross entropy loss for the main softmax prediction.
     slim.losses.sparse_softmax_cross_entropy(logits[0],
-                                 dense_labels,
-                                 label_smoothing=0.1,
-                                 weight=1.0)
+                                 dense_labels)
     # Cross entropy loss for the auxiliary softmax head.
     # slim.losses.cross_entropy_loss(logits[1],
     #                              dense_labels,
